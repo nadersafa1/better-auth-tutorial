8fe47dd8dbb77a31be1bb904487470305c886951
nadersafadev <nader@orthoplexsolutions.com>
2025-10-04 15:25:36 +0300

    add basic email verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,10 +3,31 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { db } from "@/drizzle/db";
 import * as schema from "@/drizzle/schema";
+import { sendEmailAction } from "@/actions/send-email.action";
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
+    minPasswordLength: 8,
+    requireEmailVerification: true,
+    // sendResetPassword: async ({ user,url }) => {
+    //  await sendPasswordResetEmail({user,url})
+    // },
+  },
+  emailVerification: {
+    autoSignInAfterVerification: true,
+    expiresIn: 60 * 60 * 24, // 1 day
+    sendOnSignUp: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      await sendEmailAction({
+        to: user.email,
+        subject: "Email Verification",
+        meta: {
+          description: "Click the link below to verify your email",
+          link: String(url),
+        },
+      });
+    },
   },
   sessions: {
     // cookieCache is used to enable caching of user sessions
