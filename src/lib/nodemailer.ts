a6af4f4a21e0fda9324d210e26a212f67bfa077a
nadersafadev <nader@orthoplexsolutions.com>
2025-10-09 04:15:25 +0300

    update nodemailer configuration to use environment variables for host

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: process.env.NODEMAILER_HOST,
   port: 465,
   auth: {
     user: process.env.NODEMAILER_USER,
